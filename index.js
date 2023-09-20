774cd2f342f9543a3b5f6b76e2824b0af9579c0b
Ruth <ruro122020@gmail.com>
2023-09-19 21:48:46 -0400

    rewrite problem

--- index.js
+++ index.js
@@ -14,6 +14,27 @@ function hasTargetSum(array, target) {
   Add written explanation of your solution here
 */
 
+/*
+1. Rewrite the Problem in Your Own Words
+This is one way of doing it:
+  -- The input to this function will be an array of numbers and a target. What this means is that the target is the goal 
+     that you want to return true for. So using the array of numbers, check to see if 2 numbers within that array will 
+     add up to the target number. If any 2 numbers add up to the target number, return true. If not, return false.
+     The two numbers don't have to be in any order or adjacent to each other. So we can take the first number of the
+     array, add it to the second number of the array(or the last, or the middle, it doesn't matter) and then compare
+     the sum to the target. If it doesn't match the target, take the number in the 3rd position and add it to the 
+     first number and compare it to the target again, keep doing this til you reach the end of the array for the 
+     first number and then it again for the second number til you reach the end of the array. If at any point there 
+     is a match to the target stop adding the numbers and return true. If you reach the end of the array and there 
+     never was a match return false.
+Another approach would be: 
+  -- 
+2. Write Your Own Test Cases
+3. Pseudocode
+4. Code
+5. Make It Clean and Readable
+6. Optimize
+*/
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
